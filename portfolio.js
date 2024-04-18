52084743e3402685c0a82391c1ae6ad45071170c
Archana Ojha <archana.ozza@gmail.com>
2024-04-18 22:36:36 +0300

    add media queries to make website responsive and add some js function

--- portfolio.js
+++ portfolio.js
@@ -5,11 +5,24 @@ const typed = new Typed(".auto-change", {
     loop:true
 })
 
+//for stopping the typed effect after the breakpoint
+function stop(){
+    const name = document.querySelector('h1');
+    if (window.innerWidth <= 768) {
+        typed.destroy();
+        name.innerHTML = "I'm Archana Ojha"
+    }
+}
+
+stop();
+window.addEventListener('resize', stop);
+
+//eventlistner for toggle icon
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu');
-    const navLinks = document.querySelector('.links');
+    const navLinks = document.querySelector('nav ul');
 
     menuIcon.addEventListener('click', function() {
-        navLinks.classList.toggle('menu-active');
+        navLinks.classList.toggle('active');
     });
 });

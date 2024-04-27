71955a4be1dc61e606f78284384bc5bd17be555e
Archana Ojha <archana.ozza@gmail.com>
2024-04-27 21:19:54 +0300

    made some changes

---
+++ js/mainContent.js
@@ -0,0 +1,9 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    const facebook = document.querySelector("#facebook");
+
+    facebook.addEventListener('click', function(){
+        window.open("https://www.facebook.com/archana.ojha.397/", '_blank');
+    });
+});*/
+
+
